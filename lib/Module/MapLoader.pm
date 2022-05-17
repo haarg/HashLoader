56f6dda856e86e0de73b6007298f8c45af83c3e0
Graham Knop <haarg@haarg.org>
2022-05-17 16:34:05 +0200

    docs

--- lib/Module/MapLoader.pm
+++ lib/Module/MapLoader.pm
@@ -167,12 +167,97 @@ Module::MapLoader - Load modules from predefined mapping
 
 =head1 SYNOPSIS
 
-  use Module::MapLoader;
+  use Module::MapLoader files => { "My/Module.pm" => "/my_project/lib/My/Module.pm" };
+
+  my $hook = Module::MapLoader->new(
+    files => {
+      "My/Module.pm" => "/my_project/lib/My/Module.pm",
+    },
+    mapfile_pl => [
+      '/my_project/mappingfile.pl',
+    ],
+    mapfile_tsv => [
+      '/my_project/mappingfile.tsv',
+    ],
+    lib_dir => [
+      '/my_project/lib',
+    ],
+  );
+
+  unshift @INC, $hook;
 
 =head1 DESCRIPTION
 
-Creates an L<perlvar/@INC> hook which will load modules from a predefined
+Creates an L<@INC|perlvar/@INC> hook which will load modules from a predefined
 mapping, rather than searching directories.
+
+=head1 METHODS
+
+=head2 import
+
+If any parameters are passed to import, a new hook object is created using
+those parameters, and the hook is added to the beginning of
+L<@INC|perlvar/@INC>.
+
+=head2 new
+
+Creates a new hook object.
+
+=head3 Options
+
+=over 4
+
+=item files
+
+A hashref of loadable file names to full paths. The file name should be a module
+fragment as would be passed to L<perlfunc/require>, like C<Module/MapLoader.pm>,
+not a module name, like C<Module::MapLoader>.
+
+=item mapfile_pl
+
+A mapping file, or an array reference of mapping files, of perl code returning
+a hash reference of loadable files to full paths.
+
+=item mapfile_tsv
+
+A mapping file, or an array reference of mapping files, containing a tab
+separated list of loadable files and full file paths.
+
+=item lib_dir
+
+A library directory or array reference of library directories, which will be
+recursively searched for modules to be able to load. Library directories can
+be provided as strings, or as hashrefs with the options C<path>, the directory
+to search, and C<filter>, containing a regular expression to limit which files
+to include in the final mapping. If not provided, the C<filter> defaults to
+C</\.(pl|pm)$>.
+
+=back
+
+=head2 INC
+
+When used as a hook, this method is called by perl.
+
+=head2 files
+
+Returns a hash reference of the mapping of files to full paths that are being
+provided by the hook. This method will also be used by L<Module::Pluggable> to
+see what files are available to be loaded.
+
+=head2 FILES
+
+An alias for files.
+
+=head1 CAVEATS
+
+=over 4
+
+=item *
+
+While this module may be able to load modules that use XS, that has not been
+well tested and is not a high priority feature to support.
+
+=back
 
 =head1 AUTHOR
 
